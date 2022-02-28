0fcbe08ff1175e22f01de6bef6264ede1bda2999
yan.barreiro <yan.barreiro@cubos.io>
2022-02-28 21:09:58 +0000

    feat: enable 2FA

--- src/handlers/auth.js
+++ src/handlers/auth.js
@@ -1,16 +1,20 @@
-const auth = require('../services/auth')
+const authService = require('../services/auth')
 
 const authenticate = async ctx => {
-  const { email, password } = ctx.request.body
-  const { accessToken, refreshToken, refreshTokenExpiration } = await auth.authenticate({ email, password })
-  ctx.cookies.set('refreshToken', refreshToken, { httpOnly: true, expires: refreshTokenExpiration })
-  ctx.body = {
-    accessToken,
+  const { email, password, twoFactorToken } = ctx.request.body
+  const { accessToken, refreshToken, refreshTokenExpiration, twoFactorEnabled } = await authService.authenticate({ email, password, twoFactorToken })
+  if (!accessToken && twoFactorEnabled) {
+    ctx.body = { twoFactorEnabled }
+  } else {
+    ctx.cookies.set('refreshToken', refreshToken, { httpOnly: true, expires: refreshTokenExpiration })
+    ctx.body = {
+      accessToken,
+    }
   }
 }
 
 const refreshToken = async ctx => {
-  const { accessToken, refreshToken, refreshTokenExpiration } = await auth.refreshToken({ token: ctx.cookies.get('refreshToken') })
+  const { accessToken, refreshToken, refreshTokenExpiration } = await authService.refreshToken({ token: ctx.cookies.get('refreshToken') })
   ctx.cookies.set('refreshToken', refreshToken, { httpOnly: true, expires: refreshTokenExpiration })
   ctx.body = {
     accessToken,
@@ -19,13 +23,24 @@ const refreshToken = async ctx => {
 
 const logout = async ctx => {
   const { allDevices } = ctx.request.body
-  await auth.logout({ token: ctx.cookies.get('refreshToken'), allDevices })
+  await authService.logout({ token: ctx.cookies.get('refreshToken'), allDevices })
   ctx.cookies.set('refreshToken', '')
   ctx.body = {}
+}
+
+const generateQrCode = async ctx => {
+  const qrcode = await authService.generateQrCode(ctx.state.userId)
+  ctx.body = `<img src=${qrcode}>`
+}
+const activateTwoFactor = async ctx => {
+  const { token } = ctx.request.body
+  return authService.activateTwoFactor({ userId: ctx.state.userId, token }).then(ctx.body = { activated: true })
 }
 
 module.exports = {
   authenticate,
   refreshToken,
   logout,
+  generateQrCode,
+  activateTwoFactor,
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,6 @@
+const otplib = require('otplib')
+const qrcode = require('qrcode')
+
 const users = require('./user')
 const crypto = require('./crypto')
 const tokenService = require('./token')
@@ -12,7 +15,7 @@ const authFailed = () => Promise.reject({
   message: 'Failed to authenticate',
 })
 
-const authenticate = async ({ email, password }) => {
+const authenticate = async ({ email, password, twoFactorToken }) => {
   const user = await users.findByEmail(email)
   if (!user) {
     return authFailed()
@@ -21,6 +24,17 @@ const authenticate = async ({ email, password }) => {
   if (!isMatch) {
     return authFailed()
   }
+  if (user.twoFaEnabled && !twoFactorToken) {
+    return { twoFactorEnabled: true }
+  }
+
+  if (user.twoFaEnabled) {
+    const isTwoFactorTokenValid = otplib.authenticator.verify({ secret: user.twoFaSecret, token: twoFactorToken })
+    if (!isTwoFactorTokenValid) {
+      return authFailed()
+    }
+  }
+
   const { token: refreshToken, expiresAt: refreshTokenExpiration } = await tokenService.createRefreshToken(user.id)
   return {
     refreshToken,
@@ -56,8 +70,29 @@ const logout = async ({ token, allDevices }) => {
   return tokenService.invalidateRefreshToken(token)
 }
 
+const generateQrCode = async (userId) => {
+  const secret = otplib.authenticator.generateSecret()
+
+  await users.addTwoFASecret(userId, secret)
+
+  const otpAuth = otplib.authenticator.keyuri(userId, 'Example', secret)
+
+  return qrcode.toDataURL(otpAuth)
+}
+
+const activateTwoFactor = async ({ userId, token }) => {
+  const { twoFaSecret: secret } = await users.findById(userId)
+  if (otplib.authenticator.verify({ secret, token })) {
+    await users.activateTwoFactor(userId)
+  } else {
+    return Promise.reject('Invalid token')
+  }
+}
+
 module.exports = {
   authenticate,
   refreshToken,
   logout,
+  generateQrCode,
+  activateTwoFactor,
 }
